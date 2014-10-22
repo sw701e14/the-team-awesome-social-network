b57f160a973ba6b96701ef79516384a3c2823012
deaddog <mikkelslsl@gmail.com>
2014-10-22 15:16:18 +0200

    Created Matrix struct.

---
+++ code/SocialNetworks/SocialNetworks/Matrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworks
+{
+    public struct Matrix
+    {
+        private int size;
+        private double[,] data;
+
+        public Matrix(double[,] data)
+        {
+            if (data.GetLength(0) != data.GetLength(1))
+                throw new ArgumentException("Array must be square!");
+
+            this.size = data.GetLength(0);
+
+            this.data = new double[data.GetLength(0), data.GetLength(0)];
+            for (int y = 0; y < size; y++)
+                for (int x = 0; x < size; x++)
+                    this.data[x, y] = data[x, y];
+        }
+    }
+}
