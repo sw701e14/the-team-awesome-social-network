5c199671ff7205c1a48e46db64db330dc2490d1e
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-22 13:41:54 +0200

    added a firends list to person

--- code/SocialNetworks/SocialNetworks/Person.cs
+++ code/SocialNetworks/SocialNetworks/Person.cs
@@ -8,9 +8,12 @@ namespace SocialNetworks
 {
     public class Person
     {
-        public Person(string name)
+        public Person(string name, string summary, string review)
         {
             this.name = name;
+            this.summary = summary;
+            this.review = review;
+            this.friends = new PersonCollection();
         }
 
         private string name;
@@ -20,6 +23,57 @@ namespace SocialNetworks
             get { return name; }
         }
 
+        private string summary;
+
+        public string Summary
+        {
+            get { return summary; }
+        }
+
+        private string review;
+
+        public string Review
+        {
+            get { return review; }
+        }
+
+        private PersonCollection friends;
+
+        public PersonCollection Friends
+        {
+            get { return friends; }
+        }  
+
+        
+
+        public class PersonCollection : IEnumerable<Person>
+        {
+            private List<Person> friends;
+            public PersonCollection()
+            {
+                this.friends = new List<Person>();
+            }
+
+            public void Add(Person person)
+            {
+                this.friends.Add(person);
+            }
+
+            public void Remove(Person person)
+            {
+                this.friends.Remove(person);
+            }
+
+            public IEnumerator<Person> GetEnumerator()
+            {
+                return this.friends.GetEnumerator();
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return this.friends.GetEnumerator();
+            }
+        }
 
         
     }
