103f45188f7c9985559355c0e9b730ebd3c63f3a
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-22 13:25:44 +0200

    added person class

---
+++ code/SocialNetworks/SocialNetworks/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworks
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+
+        
+    }
+}
