697f88cf781319f874adac95b1f11335531381a1
deaddog <mikkelslsl@gmail.com>
2014-10-22 15:18:59 +0200

    Moved the methods from SpectralClustering to Matrix.

--- code/SocialNetworks/SocialNetworks/Matrix.cs
+++ code/SocialNetworks/SocialNetworks/Matrix.cs
@@ -23,5 +23,26 @@ namespace SocialNetworks
                 for (int x = 0; x < size; x++)
                     this.data[x, y] = data[x, y];
         }
+
+        public static Matrix CreateIdentityMatrix(int size)
+        {
+            Matrix matrix = new Matrix() { data = new double[size, size], size = size };
+            for (int i = 0; i < size; i++)
+                matrix.data[i, i] = 1.0;
+            return matrix;
+        }
+
+        public Matrix CreateAdjacencyMatrix(Person[] persons)
+        {
+            double[,] adjacencyMatrix = new double[persons.Length, persons.Length];
+            for (int i = 0; i < adjacencyMatrix.Length; i++)
+                for (int j = 0; j < persons[i].Friends.Length; j++)
+                {
+                    int friendIndex = persons.BinarySearch(persons[i].Friends[j], (a, b) => a.Name.CompareTo(b.Name));
+                    adjacencyMatrix[i, friendIndex] = 1;
+                }
+
+            return new Matrix(adjacencyMatrix);
+        }
     }
 }
