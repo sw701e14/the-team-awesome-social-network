fad69d2acdf368d23f006c526c078129acd2b529
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-29 16:21:19 +0100

    added parser for review

--- code/SocialNetworks/SocialNetworks/Review.cs
+++ code/SocialNetworks/SocialNetworks/Review.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +9,63 @@ namespace SocialNetworks
 {
     public class Review
     {
-        private int productID;
-
-        public int ProductID
+        public Review(string productId, double score, string summary, string text)
         {
-            get { return productID; }
+            this.productId = productId;
+            this.score = score;
+            this.text = text;
+            this.summary = summary;
         }
 
-        private string review;
-
-        public string Review
+        public static IEnumerable<Review> ParseFile(string fileName)
         {
-            get { return review; }
+            string[] lines = File.ReadAllLines(fileName);
+            string[] review = new string[8];
+            int reviewCounter = 0;
+            for (int i = 0; i < lines.Count(); i++)
+            {                
+                if (lines[i] == "")
+                {
+                    reviewCounter=0;
+                    yield return createReview(review);
+                    continue;
+                }
+                review[reviewCounter] = lines[i];                
+                reviewCounter++;       
+            }
+        }
+
+        private static Review createReview(string[] review)
+        {
+            return new Review(review[0].Split(':')[1], double.Parse(review[4].Split(':')[1].Trim(),System.Globalization.CultureInfo.InvariantCulture), review[6].Split(':')[1], review[7].Split(':')[1]);
+        }        
+
+        private string productId;
+
+        public string ProductId
+        {
+            get { return productId; }
+        }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+        }
+
+        private string summary;
+
+        public string Summary
+        {
+            get { return summary; }
+        }
+
+        private double score;
+
+        public double Score
+        {
+            get { return score; }
         }
         
         
