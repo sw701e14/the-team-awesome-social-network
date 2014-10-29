b27ce69b43525fa11b475d765121307cfd287aaf
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-29 15:05:19 +0100

    renamed product to review

---
+++ code/SocialNetworks/SocialNetworks/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetworks
+{
+    public class Review
+    {
+        private int productID;
+
+        public int ProductID
+        {
+            get { return productID; }
+        }
+
+        private string review;
+
+        public string Review
+        {
+            get { return review; }
+        }
+        
+        
+    }
+}
